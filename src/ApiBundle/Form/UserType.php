47e131e7890e1f770604300a3e0f396184d19754
Alex Sima <alexandru.sima20@gmail.com>
2015-09-28 02:05:45 -0700

    working on registration emails.

---
+++ src/ApiBundle/Form/UserType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ApiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('username')
+            ->add('lastLogin')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('password')
+            ->add('salt')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ApiBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'apibundle_user';
+    }
+}
